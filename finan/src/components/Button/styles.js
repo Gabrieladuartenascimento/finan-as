c47a8a1cb8a9dcc0c2c6a1137f614fae07143fd5
Gabrieladuartenascimento <gabiduartenascimento@gmail.com>
2022-11-18 19:55:44 -0300

    be

--- finan/src/components/Button/styles.js
+++ finan/src/components/Button/styles.js
@@ -7,9 +7,10 @@ export const Button = styled.button`
   border-radius: 5px;
   width: 100%;
   cursor: pointer;
-  background-color: #046ee5;
+  background-color: teal;
   color: white;
   font-weight: 600;
   font-size: 16px;
   max-width: 350px;
 `;
+
